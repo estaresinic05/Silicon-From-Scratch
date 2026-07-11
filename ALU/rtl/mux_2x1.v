381f585583eca6e00dd0900e51e26a1090de95de
Elliot Staresinic <yellowsockem@gmail.com>
2026-07-10 21:28:41 -0500

    Update mux_2x1.v

--- ALU/rtl/mux_2x1.v
+++ ALU/rtl/mux_2x1.v
@@ -1,5 +1,5 @@
 /*******************************************************
- * Project:        RISC-V CPU Design
+ * Project:        ALU Design
  * Module:         2x1 Multiplexer
  * Author:         Elliot Staresinic
  * Date:           2026-04-29
