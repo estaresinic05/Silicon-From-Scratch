b555269ca3f557ba58e965368072fbc288de2a36
Elliot Staresinic <yellowsockem@gmail.com>
2026-07-10 21:31:40 -0500

    Update reg_file.v

--- single-cycle-cpu/rtl/reg_file.v
+++ single-cycle-cpu/rtl/reg_file.v
@@ -1,5 +1,5 @@
 /*******************************************************
- * Project:        RISC-V CPU Design
+ * Project:        RISC-V Single Cycle CPU Design
  * Module:         32-bit Register File
  * Author:         Elliot Staresinic
  * Date:           2026-05-21
