ee26d162402961304f92a19a5ed939cceabefef6
Elliot Staresinic <yellowsockem@gmail.com>
2026-07-10 21:30:53 -0500

    Update imm_gen.v

--- single-cycle-cpu/rtl/imm_gen.v
+++ single-cycle-cpu/rtl/imm_gen.v
@@ -1,5 +1,5 @@
 /*******************************************************
- * Project:        RISC-V CPU Design
+ * Project:        RISC-V Single Cycle CPU Design
  * Module:         Immediate Generator
  * Author:         Elliot Staresinic
  * Date:           2026-05-24
