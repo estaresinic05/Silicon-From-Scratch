3251e76464968020fce8ebf982b807de4ce8af6d
Elliot Staresinic <yellowsockem@gmail.com>
2026-07-10 21:30:03 -0500

    Update alu_full.v

--- single-cycle-cpu/rtl/alu_full.v
+++ single-cycle-cpu/rtl/alu_full.v
@@ -1,5 +1,5 @@
 /*******************************************************
- * Project:        RISC-V CPU Design
+ * Project:        RISC-V Single Cycle CPU Design
  * Module:         ALU Full
  * Author:         Elliot Staresinic
  * Date:           2026-05-19
