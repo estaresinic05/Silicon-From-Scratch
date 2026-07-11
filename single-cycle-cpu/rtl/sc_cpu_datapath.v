2a608e1eb28c06b4b7307f04b217d0d90e130978
Elliot Staresinic <yellowsockem@gmail.com>
2026-07-10 21:32:08 -0500

    Update sc_cpu_datapath.v

--- single-cycle-cpu/rtl/sc_cpu_datapath.v
+++ single-cycle-cpu/rtl/sc_cpu_datapath.v
@@ -1,5 +1,5 @@
 /*******************************************************
- * Project:        RISC-V CPU Design
+ * Project:        RISC-V Single Cycle CPU Design
  * Module:         Single Cycle CPU Datapath
  * Author:         Elliot Staresinic
  * Date:           2026-05-24
