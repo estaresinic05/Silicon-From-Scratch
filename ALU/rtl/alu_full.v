fa0d16bab14cbdd9a444f324a7a2811eaecbdbe3
Elliot Staresinic <yellowsockem@gmail.com>
2026-07-10 21:27:33 -0500

    Update alu_full.v

--- ALU/rtl/alu_full.v
+++ ALU/rtl/alu_full.v
@@ -1,5 +1,5 @@
 /*******************************************************
- * Project:        RISC-V CPU Design
+ * Project:        ALU Design
  * Module:         ALU Full
  * Author:         Elliot Staresinic
  * Date:           2026-05-19
