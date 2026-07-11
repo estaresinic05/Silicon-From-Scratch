149371163e7ede90ce322eeee396d8a9bc04895e
Elliot Staresinic <yellowsockem@gmail.com>
2026-07-10 21:30:44 -0500

    Update full_adder.v

--- single-cycle-cpu/rtl/full_adder.v
+++ single-cycle-cpu/rtl/full_adder.v
@@ -1,5 +1,5 @@
 /*******************************************************
- * Project:        RISC-V CPU Design
+ * Project:        RISC-V Single Cycle CPU Design
  * Module:         Full Adder
  * Author:         Elliot Staresinic
  * Date:           2026-04-29
