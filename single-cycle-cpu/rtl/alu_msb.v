1d4886c217bc15611213cf66df5316dc044541a3
Elliot Staresinic <yellowsockem@gmail.com>
2026-07-10 21:30:12 -0500

    Update alu_msb.v

--- single-cycle-cpu/rtl/alu_msb.v
+++ single-cycle-cpu/rtl/alu_msb.v
@@ -1,5 +1,5 @@
 /*******************************************************
- * Project:        RISC-V CPU Design
+ * Project:        RISC-V Single Cycle CPU Design
  * Module:         ALU MSB
  * Author:         Elliot Staresinic
  * Date:           2026-05-19
